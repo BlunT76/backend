c8dc00bf2f2d9bc7e9c8f3914b84c24db6a544c5
philippe <huhmiel@free.fr>
2019-02-01 14:50:23 +0100

    added auth

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 const env = {
-    url: 'mongodb://admin:passdustage31@ds013270.mlab.com:13270/inno',
-    dbName: 'inno'
+  url: 'mongodb://admin:passdustage31@ds013270.mlab.com:13270/inno',
+  dbName: 'inno',
 };
 
 module.exports = env;
